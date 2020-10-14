1a21f9e5690f187ba56ef89bd1a5d952a17b02b9
kangkankarmaker <kangkankarmaker00@gmail.com>
2020-10-14 15:43:26 +0600

    Lottie animation

--- app/src/main/java/com/kangkan/restapi/MainActivity.java
+++ app/src/main/java/com/kangkan/restapi/MainActivity.java
@@ -1,18 +1,31 @@
 package com.kangkan.restapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText editTextName, editTextEmail, editTextPassword,editTextConfirmPassword, editTextPhone;
-    Button reg;
+    Button btn_reg;
+
+    Animation btnAnim ;
+    NestedScrollView scrollView;
+
+    LottieAnimationView animationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +41,29 @@ public class MainActivity extends AppCompatActivity {
         editTextPassword = findViewById(R.id.edit_text_password);
         editTextConfirmPassword = findViewById(R.id.edit_text_conform_password);
         editTextPhone = findViewById(R.id.edit_text_phone);
-        reg=findViewById(R.id.button_register);
-        reg.setOnClickListener(new View.OnClickListener() {
+        animationView = findViewById(R.id.lottieAnimation);
+
+        scrollView = findViewById(R.id.layout_anim);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bottom_up);
+        scrollView.setAnimation(btnAnim);
+
+        btn_reg=findViewById(R.id.button_register);
+
+
+
+        btn_reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                registerUser();
+                if(!isConnected(MainActivity.this)) {
+                    Snackbar snackbar = Snackbar
+                            .make(scrollView, "No Internet Connection", Snackbar.LENGTH_SHORT);
+                    snackbar.show();
+                }else {
+                    registerUser();
+                }
             }
         });
+
     }
 
     private void registerUser() {
@@ -96,6 +125,24 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        btn_reg.setVisibility(View.GONE);
+        animationView.setVisibility(View.VISIBLE);
 
     }
+
+    public boolean isConnected(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting())) return true;
+            else return false;
+        } else
+            return false;
+    }
+
 }
